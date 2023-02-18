a1287c8c40249effc882034d36b63d917432e301
Andentu <severalmaz97@gmail.com>
2023-02-18 18:45:08 +0300

    Добавлен код метода ShowColor и вывод значений на экран

--- New_5_2_2/Program.cs
+++ New_5_2_2/Program.cs
@@ -4,7 +4,107 @@
     {
         static void Main(string[] args)
         {
-            
+
+
+            var favcolor = new string[3];
+            Console.WriteLine("Your favorite colors");
+            for (int i = 0; i < favcolor.Length; i++)
+            {
+
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("\nIteration {0}", i + 1);
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write("Enter your color: ");
+                favcolor[i] = ShowColor();
+            }
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("\nYour favorite colors:");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var color in favcolor)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadKey();
+        }
+        static string ShowColor()
+        {
+
+            string color = new(Console.ReadLine());
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Black; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+
+                case "yellow":
+                    Console.BackgroundColor = ConsoleColor.Yellow; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Black; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Black; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "blue":
+                    Console.BackgroundColor = ConsoleColor.Blue; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.White; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "dark blue":
+                    Console.BackgroundColor = ConsoleColor.DarkBlue; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.White; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "violet":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.White; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Black; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                case "white":
+                    Console.BackgroundColor = ConsoleColor.White; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Black; // свойство цвета шрифта.
+                    Console.WriteLine("Your color is " + color);
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow; // свойство цвета фона консоли (не всего, а того, который у наших данных).
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Sorry color {0} not found in database", color);
+                    break;
+
+
+
+
+
+
+            }
+            return color;
+
         }
     }
 }
